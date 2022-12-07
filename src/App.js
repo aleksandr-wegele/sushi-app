d4a8d497869f0b89da00076a32f6065f9292e29a
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-12-07 22:52:58 +0300

    feat: прописал стили для компонентов

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import LeftBar from "./components/leftbar/LeftBar"
+import RightBar from "./components/rightbar/RightBar"
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div className="App">
-      Hello!
+      <LeftBar/>
+      <Header/>
+      <main>
+        <div className="content__container">
+         <Home/>
+        </div>
+      </main>
+      <Footer/>
+      <RightBar/>
     </div>
   );
 }
